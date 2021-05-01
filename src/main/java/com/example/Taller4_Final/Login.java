32739822c9013db84cb6f932781acd1b087b1cb3
juanf <jfruizg@unbosque.edu.co>
2021-05-01 16:20:26 -0500

    Initial commit

---
+++ src/main/java/com/example/Taller4_Final/Login.java
@@ -0,0 +1,34 @@
+package com.example.Taller4_Final;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+
+@WebServlet(name = "loginCookies", value = "/login")
+public class Login extends HttpServlet {
+
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        response.setContentType("text/html");
+
+        String name = request.getParameter("userName");
+
+        try {
+            PrintWriter out = response.getWriter();
+
+            out.println("hello" + name);
+
+            Cookie c1 = new Cookie("userName", name);
+
+            response.addCookie(c1);
+
+            response.sendRedirect(request.getContextPath() + "/pag2.html"); // se agrego un enlcae que se dirije a otros servlet
+            out.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
